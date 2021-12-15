2c549904180fb4eb35e1ac37d8261fdc04964c88
user-penguin <kobzev96@gmail.com>
2021-12-16 01:25:18 +0700

    change start state to begin state

--- state/state.go
+++ state/state.go
@@ -3,7 +3,7 @@ package state
 type State string
 
 const (
-	Start              = "Start"
+	Begin              = "Begin" // default value means the User in main menu
 	CountWait          = "CountWait"
 	FirstCurrencyWait  = "FirstCurrencyWait"
 	SecondCurrencyWait = "SecondCurrencyWait"
