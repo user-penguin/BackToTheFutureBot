eb7d43e65244c4108464465a44f34fb425286aa5
user-penguin <kobzev96@gmail.com>
2021-12-15 22:53:07 +0700

    add messages

--- message/message.go
+++ message/message.go
@@ -1,9 +1,21 @@
 package message
 
 func StartMessage() string {
-	return "Hello"
+	return "Добро пожаловать к боту-денежному конвертеру"
 }
 
 func ConvertMessage() string {
 	return "Convert"
+}
+
+func SelectFirstCurrency() string {
+	return "Окей, погнали. Выберите первую валюту"
+}
+
+func SelectSecondCurrency() string {
+	return "Выберите вторую валюту"
+}
+
+func TypeCount(currency1 string, currency2 string) string {
+	return "Сколько " + currency1 + " вы хотите конвертировать в " + currency2 + "?"
 }
